cdb4da20a050ca8103c0d34ba9d6f06105d3f6c3
Symbosys <symbosystech@gmail.com>
2026-04-18 10:44:49 +0530

    change image url validation

--- src/api/hooks/blogs/blog.hooks.ts
+++ src/api/hooks/blogs/blog.hooks.ts
@@ -1,15 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import api from "../../api";
 import type { BlogFilters } from "../../../types/blogs/blog.types";
-
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api/v1";
 
 export const useBlogs = (params: BlogFilters = {}) => {
   return useQuery({
     queryKey: ["blogs", params],
     queryFn: async () => {
       // For website, we only fetch published blogs
-      const { data } = await axios.get(`${API_URL}/blogs`, { 
+      const { data } = await api.get("/blogs", { 
         params: { ...params, isPublished: true } 
       });
       return data.data;
@@ -21,7 +19,7 @@ export const useBlog = (idOrSlug: string) => {
   return useQuery({
     queryKey: ["blogs", idOrSlug],
     queryFn: async () => {
-      const { data } = await axios.get(`${API_URL}/blogs/${idOrSlug}`);
+      const { data } = await api.get(`/blogs/${idOrSlug}`);
       return data.data;
     },
     enabled: !!idOrSlug,
